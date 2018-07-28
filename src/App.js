a9fb555f0974cba7215f697cacfcda8bcd7b27e7
isao_e <isao0214@gmail.com>
2018-07-28 16:50:37 +0900

    src/components/HelloWorld.jsにgreetingを追加

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <HelloWorld />
+        <HelloWorld greeting={'こんばんは'}/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <h1>Hello World</h1>
+export default (props) => <h1>{props.greeting} World</h1>
