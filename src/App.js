779160ed8a1f6c3f0dd2d1c2826dd57af21c9b93
isao_e <isao0214@gmail.com>
2018-07-28 17:24:28 +0900

    引数を定数からstateにしてみた、stateの値を変更するボタンを置いてみた

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      greeting: 'Hello',
+      to: 'World',
+    };
+    this.updateGreeting = this.updateGreeting.bind(this);
+  }
+
+  updateGreeting() {
+    this.setState( {greeting: 'こんにちは'} );
+  }
   render() {
     return (
       <div className="App">
-        <HelloWorld to={'世界'}/>
+        <HelloWorld greeting={this.state.greeting} to={this.state.to}/>
+        <button onClick={this.updateGreeting}>日本語で</button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
